0afd4f89efcd1d30cef734dc1586fb3e191eac70
Costadinos Argiris <cargiris@users.noreply.github.com>
2017-09-03 15:42:34 -0600

    Updated init.pp and created new rb script for custom fact generation

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+# Class: git_version
+# ===========================
+#
+# This is a toy module that when included will simply generate a custom fact
+# that contains the version of git that is currently installed which can  then 
+# be returned using facter.
+#
+# Parameters
+# ----------
+#
+#  N/A
+#
+
+class git_version ( ) {
+
+}
